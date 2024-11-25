215d7f86692ee19dc6e50d5b1856db02cc356889
Wendel124597 <00001118338327sp@al.educacao.sp.gov.br>
2024-11-25 19:30:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o JavaScript?',
+    'O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior cidade da França?',
+    'A maior cidade da França é Paris.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa depurar um código?',
+    'Depurar um código é o processo de identificar e corrigir erros no programa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se pergunta "como você está?" em inglês?',
+    'Em inglês, "como você está?" é "how are you?".'
+)
